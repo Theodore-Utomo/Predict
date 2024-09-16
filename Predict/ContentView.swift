b82db6ee6061e8172bffcbb7ce8c7f3d2a5a96a4
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-16 17:13:10 -0400

    Added Randomized Predictions

--- Predict/ContentView.swift
+++ Predict/ContentView.swift
@@ -9,6 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var prediction = ""
+    private let choicesArray = ["It is certain",
+                        "It is decidedly so",
+                        "Without a doubt",
+                        "Yes, definitely",
+                        "You may rely on it",
+                        "As I see it, yes",
+                        "Most likely",
+                        "Outlook good",
+                        "Yes",
+                        "Signs point to yes",
+                        "Reply hazy try again",
+                        "Ask again later",
+                        "Better not tell you now",
+                        "Cannot predict now",
+                        "Concentrate and ask again",
+                        "Don't count on it",
+                        "My reply is no",
+                        "My sources say no",
+                        "Outlook not so good",
+                        "Very doubtful"]
+    
     var body: some View {
         VStack {
             Spacer()
@@ -20,9 +41,11 @@ struct ContentView: View {
             Text(prediction)
                 .font(.largeTitle)
                 .frame(height: 120)
+                .animation(.easeInOut, value: prediction)
+                .multilineTextAlignment(.center)
             Spacer()
             Button("Predict!") {
-                prediction = "Awesomeness is on its way!"
+                prediction = choicesArray.randomElement() ?? "I cannot predict right now"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
